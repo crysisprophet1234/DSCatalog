71f0d6aa09c827c9b3cc2b6a6a9c8de291a430e2
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-01-17 00:02:57 -0300

    format price

--- frontend/src/components/ProductPrice/index.tsx
+++ frontend/src/components/ProductPrice/index.tsx
@@ -1,3 +1,4 @@
+import { formatPrice } from 'utils/formatters';
 import './styles.css';
 
 type Props = {
@@ -11,7 +12,7 @@ const ProductPrice = ( { price } : Props) => {
     return (
         <div className="product-price-container">
             <span>R$</span>
-            <h3>{ price }</h3>
+            <h3>{ formatPrice(price) }</h3>
         </div>
     );
 }
