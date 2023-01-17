e633ec4a36d5a6678dec1aba720ff195846623d2
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-01-16 23:58:44 -0300

    adicionado types product

--- frontend/src/components/ButtonIcon/index.tsx
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -6,7 +6,7 @@ const ButtonIcon = () => {
   return (
     <div className="btn-container">
       <button className="btn btn-primary">
-        <h6>Inicie agora a sua busca</h6>
+        <h6>Inicie agora sua busca</h6>
       </button>
       <div className="btn-icon-container">
         <ArrowIcon />
