5717973472eb353c48b413f8acea98194851d293
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-01-19 23:32:30 -0300

    corrigido svg pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -10,12 +10,7 @@ const Pagination = () => {
             <div className="pagination-item active">1</div>
             <div className="pagination-item">2</div>
             <div className="pagination-item">3</div>
-            <div className="pagination-item">4</div>
-            <div className="pagination-item">5</div>
-            <div className="pagination-item">6</div>
-            <div className="pagination-item">7</div>
-            <div className="pagination-item">8</div>
-            <div className="pagination-item">9</div>
+            <div className="pagination-item">...</div>
             <div className="pagination-item">10</div>
             <ArrowIcon className='active'/>
         </div>
