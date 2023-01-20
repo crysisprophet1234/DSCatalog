459d410ba3ad78dcc88c5491bc285a13850b9de1
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-01-20 00:11:55 -0300

    Adicionado style responsivo navbar admin

---
+++ frontend/src/pages/Admin/Navbar/index.tsx
@@ -0,0 +1,31 @@
+import './Navbar.css';
+
+const Navbar = () => {
+
+    return (
+
+        <nav className="admin-nav-container">
+
+            <ul>
+
+                <li>
+                    <a href='link' className='admin-nav-item active'>Produtos</a>
+                </li>
+
+                <li>
+                    <a href='link' className='admin-nav-item'>Categorias</a>
+                </li>
+
+                <li>
+                    <a href='link' className='admin-nav-item'>Usuários</a>
+                </li>
+
+            </ul>
+
+        </nav>
+
+    )
+
+}
+
+export default Navbar;
--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,5 +1,24 @@
+import Navbar from "./Navbar";
+import './Admin.css';
+
 const Admin = () => {
-  return <h1>Página Admin</h1>;
-};
+
+  return (
+
+    <div className="admin-container">
+
+      <Navbar />
+
+      <div className="admin-content">
+
+        <h1>Content</h1>
+
+      </div>
+
+    </div>
+
+  )
+
+}
 
 export default Admin;
