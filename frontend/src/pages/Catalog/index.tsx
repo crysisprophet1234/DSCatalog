52ea3a8d8ab6b7ae102275a467837a71b38b7c84
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-01-21 03:16:50 -0300

    Catalog dynamic rendering added

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -35,21 +35,6 @@ const Catalog = () => {
 
   }, [])
 
-  const product: Product = { //teste
-    id: 2,
-    name: "Smart TV",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    price: 2190.0,
-    imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
-    date: "2020-07-14T10:00:00Z",
-    categories: [
-      {
-        id: 1,
-        name: "Livros"
-      }
-    ]
-  }
-
   return (
 
     <div className="container my-4 catalog-container">
@@ -60,35 +45,19 @@ const Catalog = () => {
 
       <div className="row">
 
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
+        {page?.content.map(product => {
 
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/2">
-            <ProductCard product={product} />
-          </Link>
-        </div>
+          return (
 
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/3">
-            <ProductCard product={product} />
-          </Link>
-        </div>
+            <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
+              <Link to={`products/${product.id}`}>
+                <ProductCard product={product} />
+              </Link>
+            </div>
 
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/4">
-            <ProductCard product={product} />
-          </Link>
-        </div>
+          );
 
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/5">
-            <ProductCard product={product} />
-          </Link>
-        </div>
+        })}
 
       </div>
 
