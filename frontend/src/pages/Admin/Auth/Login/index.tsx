ed258062f398f52dba6ada99760a359d6e360125
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-02-18 22:43:05 -0300

    Adicionado validacao e estilizacao login input

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -17,7 +17,7 @@ const Login = () => {
 
     const [hasError, setHasError] = useState(false);
 
-    const { register, handleSubmit } = useForm<FormData>();
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
     const onSubmit = (formData: FormData) => {
 
@@ -54,24 +54,38 @@ const Login = () => {
                 <div className="mb-4">
 
                     <input
-                        {...register("username")}
+                        {...register("username", {
+                            required: 'Campo obrigatório',
+                            pattern: {
+                                value: (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i),
+                                message: 'Email inválido'
+                            }
+                        })}
                         type="text"
-                        className="form-control base-input"
+                        className={`form-control base-input ${errors.username ? 'is-invalid' : ''}`}
                         placeholder="Email"
                         name="username"
                     />
+                    <div className="invalid-feedback d-block">
+                        {errors.username?.message}
+                    </div>
 
                 </div>
 
                 <div className="mb-2">
 
                     <input
-                        {...register("password")}
+                        {...register("password", {
+                            required: 'Campo obrigatório'
+                        })}
                         type="password"
-                        className="form-control base-input "
+                        className={`form-control base-input ${errors.password ? 'is-invalid' : ''}`}
                         placeholder="Password"
                         name="password"
                     />
+                    <div className="invalid-feedback d-block">
+                        {errors.password?.message}
+                    </div>
 
                 </div>
 
